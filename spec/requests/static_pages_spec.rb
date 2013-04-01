7614c37851aa8bbda16ee9b90af28d37a7cf5d3f
Connor Mendenhall <ecmendenhall@gmail.com>
2013-04-01 14:50:18 -0700

    Refactored StaticPages tests.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,20 +1,33 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+    subject { page }
+    
+    shared_examples_for "all static pages" do
+        it { should have_content(heading) }
+        it { should have_title(full_title(page_title)) }
+    end
+
     describe "Home page" do
         before { visit root_path }
-        subject { page }
+        let(:heading) { 'Pushpin' }
+        let(:page_title) { '' }
 
-        it { should have_content('Pushpin') }
-        it { should have_title(full_title('')) }
+        it_should_behave_like "all static pages" 
         it { should_not have_title(full_title('Home')) }
+        it "should have the right links on the layout" do
+            click_link "About"
+            expect(page).to have_title(full_title('About'))
+        end
     end
 
     describe "About page" do
         before { visit about_path }
-        subject { page }
+        let(:heading) { 'About Pushpin' }
+        let(:page_title) { 'About' }
 
-        it { should have_title(full_title('About')) }
-        it { should have_content('About Pushpin') }
+        it_should_behave_like "all static pages"
     end
+
+
 end
